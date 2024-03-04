4a3f07ca7c5ff7f72ef28e7d3a9d20c89485a1ce
riadsarkar45 <riadsarkar45@gmail.com>
2024-03-04 22:57:06 +0600

    footer/serverSideErrorFixed

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -32,16 +32,16 @@ const Login = () => {
                     <div className="grid grid-cols-1 bg-white bg-opacity-20 shadow-md p-2 rounded-md">
                         <div>
                             <h2>Email</h2>
-                            <input name="email" type="email" className="bg-white text-black p-3 mt-2 w-[30rem] rounded-md"  />
+                            <input name="email" type="email" className="bg-white text-black p-3 mt-2 lg:w-[30rem] w-full rounded-md"  />
                         </div>
                         <div>
                             <h2>Password</h2>
-                            <input name="password" type="password" className="bg-white text-black p-3 mt-2 w-[30rem] rounded-md" />
+                            <input name="password" type="password" className="bg-white text-black p-3 mt-2 lg:w-[30rem] w-full rounded-md" />
                         </div>
                         <div className="text-center mt-7 ">
                             <button className="bg-green-500 shadow-[red] w-[13rem] bg-opacity-20 border border-green-500 rounded-md p-2">Login</button>
                         </div>
-                        <p className="mt-3">Don't have an account please <Link className="text-blue-900" to="/register">Register</Link></p>
+                        <p className="mt-3">Don{"'"}t have an account please <Link className="text-blue-900" to="/register">Register</Link></p>
                     </div>
                 </form>
 
--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -6,7 +6,7 @@ import { AuthContext } from "../Auth/AuthProvider/AuthProvider";
 
 const axiosSecure = axios.create({
     //baseURL: 'http://localhost:5000'
-     baseURL: 'https://server-side-bice.vercel.app'
+     baseURL: 'https://helthcare.vercel.app'
 })
 const useAxiosSecure = () => {
     const navigate = useNavigate();
